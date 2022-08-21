1343dde64328d558274430280425fe192b13ec42
juan pablo sanchez <juansanchezg@javeriana.edu.co>
2022-08-21 00:03:27 -0500

    login/welcome/main/olvidar
    pantallas hechas falta la parte de conexion entre pantallas

--- app/src/main/java/com/example/petplanet/LoginActivity.java
+++ app/src/main/java/com/example/petplanet/LoginActivity.java
@@ -2,7 +2,15 @@ package com.example.petplanet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +18,77 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        EditText password = findViewById(R.id.editTextPassword);
+        EditText correo = findViewById(R.id.editTextCorreo);
+        Button login = findViewById(R.id.iniciosesionBTN);
+        TextView olvido = findViewById(R.id.olvidocontra);
+        TextView registro = findViewById(R.id.nuevousuario);
+
+        registro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, SelecciondeCuentaActivity.class);
+                startActivity(intent);
+
+                finish();
+            }
+        });
+
+        olvido.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, OlvidarpasswordActivity.class);
+                startActivity(intent);
+
+                finish();
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                password.setError(null);
+                correo.setError(null);
+
+                if(isEmail(correo) == false){
+                    correo.setError("Introduce un correo electonico valido");
+                    correo.requestFocus();
+                    return;
+                }if(password.getText().toString().isEmpty()){
+                    password.setError("Introduce una contraseña");
+                    password.requestFocus();
+                    return;
+                }else{
+
+                    /*
+                    se tiene que verificar si un usuario es petowner o petwalker con el correo electronico para definir a que pantalla enviar al usuario
+
+                    por esta primera entrega vamos a mandarlo a la petowner activity
+                     */
+                    Intent intent = new Intent(LoginActivity.this, LandingPetOwnerActivity.class);
+                    intent.putExtra("correo", correo.getText().toString());
+                    intent.putExtra("password", password.getText().toString());
+                    startActivity(intent);
+                    finish();
+                }
+            }
+
+
+
+
+        });
+
+
     }
+
+    @Override
+    public void onBackPressed() {
+        finish();
+    }
+    public boolean isEmail(EditText text) {
+        CharSequence email = text.getText().toString();
+        return (!TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches());
+    }
+
 }
--- app/src/main/java/com/example/petplanet/MainActivity.java
+++ app/src/main/java/com/example/petplanet/MainActivity.java
@@ -2,16 +2,37 @@ package com.example.petplanet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Window;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
+
+
+        Button crearcuenta = findViewById(R.id.creacruentaBTN);
+        Button iniciarsesion = findViewById(R.id.iniciarsesionBTN);
+        crearcuenta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SelecciondeCuentaActivity.class);
+                startActivity(intent);
+            }
+        });
+        iniciarsesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
