dee39e2cd5ff68220b35295e92be4cd854221a3d
Daniel Castellanos <78168171+R4DRHunt@users.noreply.github.com>
2022-08-23 16:36:18 -0500

    Ambas Pantallas Landing

--- app/src/main/java/com/example/petplanet/LandingPetOwnerActivity.java
+++ app/src/main/java/com/example/petplanet/LandingPetOwnerActivity.java
@@ -2,7 +2,11 @@ package com.example.petplanet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class LandingPetOwnerActivity extends AppCompatActivity {
 
@@ -10,5 +14,32 @@ public class LandingPetOwnerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_pet_owner);
+
+        ImageButton mascotas = findViewById(R.id.mascotas);
+        ImageButton agendar = findViewById(R.id.agendarPaseoA);
+        ImageButton perfil = findViewById(R.id.perfilA);
+
+        mascotas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ListarPerrosActivity.class);
+                startActivity(intent);
+            }
+        });
+        agendar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), AgendarPaseosActivity.class);
+                startActivity(intent);
+            }
+        });
+        perfil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), PerfilUsuarioActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
